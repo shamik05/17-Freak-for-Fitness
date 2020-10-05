00e0161721e1fbfb1215fd67fc90e3784387c94f
Shamik H <shamik05@hotmail.com>
2020-10-05 16:05:58 -0400

    heroku

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 // Import dependencies
-// require("dotenv").config();
-const compression = require("compression");
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-// const path = require("path");
-// const db = require("./models");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(compression());
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
